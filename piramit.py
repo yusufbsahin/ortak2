10c3498c062d7c8bfafe526e7e640b55bfe9697b
ibrahim demir <dmrribrahimm@gmail.com>
2023-07-31 16:22:11 +0300

    hadi

--- piramit.py
+++ piramit.py
@@ -1,10 +1,6 @@
-while True:
-    try:
-        n = int(input("Bir sayı girin: "))
-        if n <= 0 or n % 2 == 0:
-            raise ValueError("Lütfen pozitif ve tek bir sayı girin.")
-        break
-    except ValueError as ve:
-        print("Hatalı giriş! " + str(ve))
+try:
+    n = int(input("Bir sayı girin: "))
+    print("\n".join((i*"*").center(n*2) for i in range(1, n*2, 2)))
+except ValueError:
+    print("Hata: Geçerli bir sayı girmediniz.")
 
-print("\n".join((i * "*").center(n * 2) for i in range(1, n * 2, 2)))
