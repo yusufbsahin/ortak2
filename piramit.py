97f077574e28a2684671d328c0e30ff7c0004e65
ibrahim demir <dmrribrahimm@gmail.com>
2023-07-31 15:16:12 +0300

    döngüye sokuluyor

--- piramit.py
+++ piramit.py
@@ -1,3 +1,10 @@
-n = int(input("Bir sayı girin: "))
+while True:
+    try:
+        n = int(input("Bir sayı girin: "))
+        if n <= 0 or n % 2 == 0:
+            raise ValueError("Lütfen pozitif ve tek bir sayı girin.")
+        break
+    except ValueError as ve:
+        print("Hatalı giriş! " + str(ve))
 
-print("\n".join((i*"*").center(n*2) for i in range(1, n*2, 2)))
+print("\n".join((i * "*").center(n * 2) for i in range(1, n * 2, 2)))
