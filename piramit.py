95a80547b15a65f8df15cc362a77b1658642293b
yusufbsahin <yusufbsahin1907@gmail.com>
2023-07-31 15:10:43 +0300

    valuerror eklendi

--- piramit.py
+++ piramit.py
@@ -1,3 +1,6 @@
-n = int(input("Bir sayı girin: "))
+try:
+    n = int(input("Bir sayı girin: "))
+    print("\n".join((i*"*").center(n*2) for i in range(1, n*2, 2)))
+except ValueError:
+    print("Hata: Geçerli bir sayı girmediniz.")
 
-print("\n".join((i*"*").center(n*2) for i in range(1, n*2, 2)))
